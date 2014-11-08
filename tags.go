8cbe80809d7e40e6df15ee2252118c7f7b210f9f
Igor Dolzhikov <bluesriverz@gmail.com>
2014-11-09 00:29:22 +0600

    typos

--- tags.go
+++ tags.go
@@ -42,41 +42,41 @@ Example:
 
 	func main() {
 		product := Product{
-			Name:        "the tee",
-			Description: "the boutle of ice black tee with sugar",
+			Name:        "the tea",
+			Description: "the boutle of ice black tea with sugar",
 			Tags:        tags.Tags{"ice", "black", "sugar"},
 		}
 
 		fmt.Println("Product:", product.Description)
 
-		// We ask for any tee "black" or "green"
-		fmt.Println("Is this tee black or green?")
+		// We ask for any tea "black" or "green"
+		fmt.Println("Is this tea black or green?")
 		query := tags.Tags{"black", "green"}
 
 		if product.Tags.IsTagged(query) {
-			fmt.Println("Yes, the tee is black.")
+			fmt.Println("Yes, the tea is black.")
 		} else {
-			fmt.Println("No, the tee has not black or green options.")
+			fmt.Println("No, the tea has not black or green options.")
 		}
 
 		// We ask fot strict match "green" and "sugar"
-		fmt.Println("Is this tee green with sugar?")
+		fmt.Println("Is this tea green with sugar?")
 		query = tags.Tags{"+green", "+sugar"}
 
 		if product.Tags.IsTagged(query) {
-			fmt.Println("Yes, the tee is green with sugar.")
+			fmt.Println("Yes, the tea is green with sugar.")
 		} else {
-			fmt.Println("No, the tee with sugar, but is not green.")
+			fmt.Println("No, the tea with sugar, but is not green.")
 		}
 
 		// We ask for strict mismatch, not "ice"
-		fmt.Println("Is this tee hot?")
+		fmt.Println("Is this tea hot?")
 		query = tags.Tags{"-ice"}
 
 		if product.Tags.IsTagged(query) {
-			fmt.Println("Yes, the tee is hot.")
+			fmt.Println("Yes, the tea is hot.")
 		} else {
-			fmt.Println("No, it is ice tee.")
+			fmt.Println("No, it is ice tea.")
 		}
 	}
 
